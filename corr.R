e1ea8e06907abd0fe41342ae2c63ed89a820cbf0
Matt Lutrick <Matt.Lutrick@s3sleepcoaches.com>
2015-02-06 14:24:38 -0600

    class:2 week:1 problem:2

---
+++ corr.R
@@ -0,0 +1,14 @@
+corr <- function(directory, threshold = 0, id = 1:332) {
+  holder <- c()
+  corr <- numeric()
+  for(i in id) {
+    current <- read.csv(paste(c(directory, "/", sprintf("%03d", i), ".csv"), collapse=""))
+    comcases <- sum(complete.cases(current))
+    if(comcases > threshold) {
+      cases <- current[complete.cases(current),]
+      casecorr <- cor(cases['nitrate'], cases['sulfate'])
+      corr <- c(corr, casecorr)
+    }
+  }
+  corr
+}
