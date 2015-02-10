ff8ba6ba2de2ebcb9bc118db0b5cb46ddf7debfa
Matt Lutrick <Matt.Lutrick@s3sleepcoaches.com>
2015-02-10 09:37:46 -0600

    matrix inverse cache problem

--- corr.R
+++ corr.R
@@ -1,11 +1,10 @@
 corr <- function(directory, threshold = 0, id = 1:332) {
-  holder <- c()
   corr <- numeric()
   for(i in id) {
     current <- read.csv(paste(c(directory, "/", sprintf("%03d", i), ".csv"), collapse=""))
-    comcases <- sum(complete.cases(current))
-    if(comcases > threshold) {
-      cases <- current[complete.cases(current),]
+    comcases <- complete.cases(current)
+    if(sum(comcases) > threshold) {
+      cases <- current[comcases, c('nitrate', 'sulfate')]
       corr <- c(corr, cor(cases['nitrate'], cases['sulfate']))
     }
   }
